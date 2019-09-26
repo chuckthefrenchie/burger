c3b5f16b7a706d403813fa67644b9258a98cc169
Daria Korbysheva <korbyshevadaria@gmail.com>
2019-09-26 15:55:03 -0700

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 var express = require("express");
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
